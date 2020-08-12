b2eafcd62756b0947309cc15bc7b0c5c50344c48
a13h0sj <69475332+a13h0sj@users.noreply.github.com>
2020-08-12 13:39:38 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@ const express =  require('express');
 const app = express();
 app.get('*',(req,res)=>{
     console.log('entro al sistema');
-    res.
+    res.send({mensaje:'Entro hasta el fondo'})
 })
+module.exports = app;
