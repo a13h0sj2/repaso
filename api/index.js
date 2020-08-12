3edd62451a1847978a64520a7a38e8b71f503009
a13h0sj <69475332+a13h0sj@users.noreply.github.com>
2020-08-12 15:24:11 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express =  require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true});
 
 const Users = mongoose.model('User', new mongoose.Schema({
                                         name:String
                                     })
                             );
-Users.create({name:'Alejandro Silva'})
+Users.create({name:'Daniel Celi'})
 app.get('*',(req,res)=>{
     Users.find()
         .then(x=>res.send(x));
