ee16d68a2a038844af3fef502fb24eacb0fedc9d
a13h0sj2 <69521304+a13h0sj2@users.noreply.github.com>
2020-08-15 15:03:39 -0500

    sad
    sad

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 const express =  require('express');
-const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true});
 
-const Users = mongoose.model('User', new mongoose.Schema({
-                                        name:String
-                                    })
-                            );
-Users.create({name:'Daniel Celi'})
-app.get('*',(req,res)=>{
-    Users.find()
-        .then(x=>res.send(x));
+app.use('/api/meals',meals)
+app.use('/api/orders',orders)
     
-})
 module.exports = app;
--- api/models/Meals.js
+++ api/models/Meals.js
@@ -1,8 +1,8 @@
-const mongoose = requiere('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Meals = mongoose.model('Meal', new Schema({
-    name:String,
-    desc:String,
-}));
+const Meals = mongoose.model('Order', new Schema({
+   name:String,
+   desc:String,
+}))
 module.exports = Meals;
