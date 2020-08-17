bfbfab1955146d6f35e24e65ee67a6b0676c135a
a13h0sj <69475332+a13h0sj@users.noreply.github.com>
2020-08-17 15:51:55 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,6 +10,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true});
+const pruebaC = mongoose.model('User', new mongoose.Schema({name:String}))
+pruebaC.create({name:'entro'})
 
 app.use('/api/meals',meals)
 app.use('/api/orders',orders)
