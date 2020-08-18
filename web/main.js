beb37a0e86d66dc564b96eaddf7c7599a6462372
a13h0sj2 <69475332+a13h0sj@users.noreply.github.com>
2020-08-18 17:09:40 -0500

    sazda
    adws

--- web/main.js
+++ web/main.js
@@ -1,6 +1,16 @@
+const renderItem = (item) => {
+    return `<li data-id="${item._id}">${item.name}</li>`
+}
+
 window.onload=()=>{
     fetch('https://repaso-git-master.a13h0sj2.vercel.app/api/meals')
     .then(response => response.json())
-    .then(data => console.log(data))    
+    .then(data => {
+        const mealsList = document.getElementById('meals-list');
+        const submit = document.getElementById('submit')
+        const template = data.map(renderItem).join('')
+        mealsList.innerHTML = template;
+        submit.removeAttribute('disabled')
+    })    
     
 }
