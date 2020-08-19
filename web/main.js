e601f27aa98bdaef38e7387df24a95bd39c550f6
a13h0sj2 <69475332+a13h0sj@users.noreply.github.com>
2020-08-19 17:21:14 -0500

    asd
    asd

--- web/main.js
+++ web/main.js
@@ -1,16 +1,25 @@
+const { json } = require("body-parser");
+
+//renderizar los items traidos de meals
 const renderItem = (item) => {
     return `<li data-id="${item._id}">${item.name}</li>`
 }
 
 window.onload=()=>{
+
+    // Enviando peticion de json de meals
     fetch('https://repaso-git-master.a13h0sj2.vercel.app/api/meals')
+    //promesa en la cual se guarda lo que responde en un json
     .then(response => response.json())
+    //data es la respuesta de la promesa 
     .then(data => {
         const mealsList = document.getElementById('meals-list');
         const submit = document.getElementById('submit')
+        //mapeamos la respuesta y guardamos en un template
         const template = data.map(renderItem).join('')
+        //cambiamos el contenido del meal-list
         mealsList.innerHTML = template;
         submit.removeAttribute('disabled')
     })    
     
-}
+}  
