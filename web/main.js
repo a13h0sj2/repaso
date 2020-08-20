1bf28eb8860a38b9dfe135caa542031e5806a283
a13h0sj2 <69475332+a13h0sj@users.noreply.github.com>
2020-08-19 19:28:34 -0500

    sd
    sad

--- web/main.js
+++ web/main.js
@@ -1,22 +1,24 @@
 //renderizar los items traidos de meals
 const renderItem = (item) => {
     //tomamos el id desde item de la variable data de la funcion fetch y el nombre del meal
-    return `<li user_id="Ricardo" meal_id="${item._id}" onclick="">${item.name}</li>`
-}
-const pedidoMenu = (usuario,meal)=>{
-    
+    return `<li meal_id="${item._id}" onclick="agregarPedido('${item.name}')">${item.name}</li>`
 }
 
+function agregarPedido(argumento){
+    const lista = document.getElementById('menu-plano')
+    const item = document.createElement('li')
+    const arg = document.createTextNode(argumento)
+    item.appendChild(arg)
+    lista.appendChild(item)
+}
 window.onload=()=>{
-
-
-
     // Enviando peticion de json de meals
     fetch('https://repaso-git-master.a13h0sj2.vercel.app/api/meals')
     //promesa en la cual se guarda lo que responde en un json
     .then(response => response.json())
     //data es la respuesta de la promesa 
     .then(data => {
+        toString
         const mealsList = document.getElementById('meals-list');
         const submit = document.getElementById('submit')
         //mapeamos la respuesta y guardamos en un template
@@ -24,6 +26,5 @@ window.onload=()=>{
         //cambiamos el contenido del meal-list
         mealsList.innerHTML = template;
         submit.removeAttribute('disabled')
-    })    
-    
+    })      
 }  
