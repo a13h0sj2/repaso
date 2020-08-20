8df6fd5bf9fca42603c9af85ef163797f3697c04
a13h0sj2 <69475332+a13h0sj@users.noreply.github.com>
2020-08-20 18:43:50 -0500

    sad
    sad

--- web/main.js
+++ web/main.js
@@ -4,12 +4,48 @@ const renderItem = (item) => {
     return `<li meal_id="${item._id}" onclick="agregarPedido('${item.name}')">${item.name}</li>`
 }
 
+//creamos un objeto HTML tipo Lista <select> y lo rellenamos 20 veces de opciones del 1 al 20
+function crearLista(){
+    const listaDesplegable = document.createElement('select')    
+    for(i=1;i<=20;i++){
+        const items = document.createElement('option')
+        const itemsCont = document.createTextNode(i);
+        items.appendChild(itemsCont);
+        listaDesplegable.appendChild(items);
+    }
+    return listaDesplegable;
+}
 function agregarPedido(argumento){
+    //creamos un objeto tipo html ,li> y le ponemos  un el texto que traemos del evento mouse click
     const lista = document.getElementById('menu-plano')
     const item = document.createElement('li')
     const arg = document.createTextNode(argumento)
     item.appendChild(arg)
-    lista.appendChild(item)
+    //revisamos si es el primer elemento a agregar
+    //para a;adir el <li> y la lista <select>
+    if (lista.firstChild==null){
+        const esperando = document.getElementById('esperando')
+        esperando.remove();
+        lista.appendChild(item);
+        lista.appendChild(crearLista());
+    }else{    //De no ser el primer elemento revisamos que no se repita para a;adir otro meals    
+        const nuevaLista = document.querySelectorAll('#menu-plano li')        
+        const array = Array.from(nuevaLista);
+        var bandera = 0;
+        for(var i = 0; i < array.length;i++){
+            if (array[i].firstChild.textContent === argumento){
+                bandera=1
+                break;
+                //el codigo salta si se encuentra repetido
+            }}
+        if (bandera==0){
+            //si no esta repetido creamos un nuevo elemento y su respectiva lista
+            lista.appendChild(item);
+            lista.appendChild(crearLista());
+        }else{
+            alert('ya pidio ese plato huevonaso')
+        }
+    }
 }
 window.onload=()=>{
     // Enviando peticion de json de meals
